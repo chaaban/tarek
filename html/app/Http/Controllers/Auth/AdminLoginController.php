f4660f2e0e409bcdc28069ed622b3d4f429efddf
Tarek Chaaban <chaabant@gmail.com>
2017-06-09 18:37:04 -0400

    Admin Login Controller Info

--- html/app/Http/Controllers/Auth/AdminLoginController.php
+++ html/app/Http/Controllers/Auth/AdminLoginController.php
@@ -7,13 +7,38 @@ use App\Http\Controllers\Controller;
 
 class AdminLoginController extends Controller
 {
+    
+    public function __construct(){
+        $this->middleware('guest:admin');
+    }
+    
+    
     public function showLoginForm() {
         return view('auth.admin-login');
     }
 
 
     public function login() {
-        return true;
+        
+        // validate the form data
+        $this->validate($request , [
+              'email'    => 'required|email',
+              'password' => 'required|min:6'
+        ]);
+        
+        // Attempt to log the user in
+        // remember is optional
+        // Auth::guard('admin')->attempt($credentials,$remember);
+        if(Auth::guard('admin')->attempt(['email' => $request->email , 'password' => $request->password] , $request->remember)) {
+          // if Success , then redirect to their location
+          // if intended fail (where they came from) -> they will be redirected to admin.dashboard
+          return redirect()->intended(route('admin.dashboard'));  
+        }    
+        // if unsuccessfull , then redirect to the login with the form data 
+        else {
+          // return to the form , with the input except ..
+          return redirect()->back()->withInput($request->only('email', 'remember')); 
+        }
     }
 
 }
