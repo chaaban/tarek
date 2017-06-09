3e8e7bcdac828da0c9c185127975945bed14cb44
Tarek Chaaban <chaabant@gmail.com>
2017-06-09 18:06:43 -0400

    Adding the admin login form / routes

--- html/app/Http/Controllers/Auth/AdminLoginController.php
+++ html/app/Http/Controllers/Auth/AdminLoginController.php
@@ -7,5 +7,13 @@ use App\Http\Controllers\Controller;
 
 class AdminLoginController extends Controller
 {
-    //
+    public function showLoginForm() {
+        return view('auth.admin-login');
+    }
+
+
+    public function login() {
+        return true;
+    }
+
 }
--- html/routes/web.php
+++ html/routes/web.php
@@ -18,5 +18,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'AdminController@index')->name('admin');
+
+
+// Since they all start with /admin ... let's group them
+Route::prefix('admin')->group(function(){
+    
+    Route::get('/login' , 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login' , 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    // Be sure to have that one at the end ... they will be caught if it's placed first 
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
+});
+
 
