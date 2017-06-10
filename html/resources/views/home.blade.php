840fa5bdbbb785797c06f1dbf5c656597f648f39
Tarek Chaaban <chaabant@gmail.com>
2017-06-10 18:03:23 -0400

    adding components

--- html/resources/views/home.blade.php
+++ html/resources/views/home.blade.php
@@ -8,7 +8,9 @@
                 <div class="panel-heading">User Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    @component('components.who')
+                    
+                    @endcomponent
                 </div>
             </div>
         </div>
